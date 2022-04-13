1e4cbdb0cd01ed383b0ac6efd5169072a7fcf82d
nideeee <liujiazuo@cdfinger.com>
2022-04-13 10:41:35 +0800

    this is test

---
+++ memory.h
@@ -0,0 +1,21 @@
+#ifndef __MEMORY_H__
+#define __MEMORY_H__
+
+#include "stdio.h"
+#include "string.h"
+#include "includes.h"
+
+//用户使用
+typedef struct
+{
+    void    *addr;      //申请到的内存的起始地址
+    uint32_t size;      //申请到的内存的大小，按照块大小分配，大于等于申请大小
+    uint16_t  tb;       //申请表序号，申请内存时分配，释放内存时使用，用户不使用
+}DMEM;
+
+//若返回空，则申请失败
+DMEM *DynMemGet(uint32_t size);
+
+void DynMemPut(DMEM *pDmem);
+
+#endif //__MEMORY_H__
